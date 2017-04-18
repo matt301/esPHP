532e775e1282285860fb1d11e906f3ae22956509
matt301 <s3911811@studenti.unige.it>
2017-04-18 15:30:29 +0200

    dadadasdad\\xa

--- regist.php
+++ regist.php
@@ -11,12 +11,14 @@ $gender = $_POST['gender'];
 $email = $_POST['email'];
 $pass = $_POST['password'];
 
+$pass_hash = password_hash($pass, PASSWORD_BCRYPT);
+
 
 $f = fopen("db.txt", 'a');
 if(!$f) die ("Errore nella operazione con il file");
 
 fwrite($f, $email.'<br>');
-fwrite($f, $pass.'<br>');
+fwrite($f, $pass_hash.'<br>');
 fwrite($f, $nome.'<br>');
 fwrite($f, $cognome.'<br>');
 fwrite($f, $gender.'<br>');
