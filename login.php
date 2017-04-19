0a8b75dbe09e9c5f946173dda5b77be381dbd650
Matteo Rossi <s3911811@studenti.unige.it>
2017-04-19 12:00:09 +0200

    bv

--- login.php
+++ login.php
@@ -5,6 +5,8 @@
  * Date: 18/04/17
  * Time: 15:47
  */
+ini_set("auto_detect_line_endings", true);
+
 
 $user = $_POST['user'];
 $pass = $_POST['password'];
@@ -12,9 +14,9 @@ $pass = $_POST['password'];
 $f = fopen("db.txt", "r");
 
 while (!feof($f)){
-    echo fgets($f)." while";
+
     if($user==fgets($f)){
-        echo fgets($f)." user";
+
         if(password_verify($pass,fgets($f))){
             echo "Welcome! ". $user;
             break;
