f4ffb89e8ee2a39300edb258052f8db3c82fe6ab
Matteo Rossi <s3911811@studenti.unige.it>
2017-04-19 10:42:44 +0200

    ci

--- login.php
+++ login.php
@@ -12,10 +12,9 @@ $pass = $_POST['password'];
 $f = fopen("db.txt", "r");
 
 while (!feof($f)){
-    echo "ciao";
-
+    echo fgets($f)." while";
     if($user==fgets($f)){
-
+        echo fgets($f)." user";
         if(password_verify($pass,fgets($f))){
             echo "Welcome! ". $user;
             break;
